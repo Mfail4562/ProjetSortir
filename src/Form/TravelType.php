5eb92fc21f82689a99afdb1085a84f8f61b9597f
Jammilla <jamila.aitazza2022@campus-eni.fr>
2023-05-16 22:04:23 +0200

    quelque modif sur la page creer sortie!

--- src/Form/TravelType.php
+++ src/Form/TravelType.php
@@ -2,12 +2,19 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Place;
 use App\Entity\Status;
 use App\Entity\Travel;
+use Doctrine\DBAL\Types\DateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class TravelType extends AbstractType
 {
@@ -15,21 +22,35 @@ class TravelType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('dateStart')
-            ->add('duration')
+            ->add('dateStart',DateTimeType::class, [
+                'date_label' => 'Start on'])
+            ->add('duration',ChoiceType::class, [
+                'choices' => [
+                    '30 minutes' => 30,
+                    '60 minutes' => 60,
+                    '90 minutes' => 90]
+                    ])
             ->add('limitDateSubscription')
             ->add('nbMaxTraveler')
+
             ->add('infos')
-            ->add('leader')
-            ->add('subscriptionedTravelers')
+           // ->add('leader')
+            //->add('subscriptionedTravelers')
             ->add('status', EntityType::class,[
                 'class' => Status::class,
                 'choice_label' => 'wording',
                 'placeholder' => '--Choice any status--',
-
             ])
-            ->add('campusOrganiser')
-            ->add('place')
+
+           // ->add('campusOrganiser', EntityType::class,[
+               // 'class' => Campus::class,
+              //  'choice_label' => 'name'
+
+           // ])
+            ->add('place',EntityType::class, [
+                'class' => Place::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
 
