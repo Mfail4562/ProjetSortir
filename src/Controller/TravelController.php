83d41174d611486442be1e2c9ca6b7aaf390351b
mathieu <mathieu.perin.prog@gmail.com>
2023-05-17 17:36:29 +0200

    complet system unregister

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -15,10 +15,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/travel')]
+#[Route('/travel', name: 'app_travel_')]
 class TravelController extends AbstractController
 {
-    #[Route('/', name: 'app_travel_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(TravelRepository $travelRepository): Response
     {
 
@@ -28,7 +28,7 @@ class TravelController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_travel_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, TravelRepository $travelRepository): Response
     {
         $user = $this->getUser();
@@ -53,7 +53,7 @@ class TravelController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_travel_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Travel $travel): Response
     {
         return $this->render('travel/show.html.twig', [
@@ -62,7 +62,7 @@ class TravelController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_travel_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Travel $travel, TravelRepository $travelRepository): Response
     {
         $form = $this->createForm(TravelType::class, $travel);
@@ -80,7 +80,7 @@ class TravelController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_travel_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Travel $travel, TravelRepository $travelRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $travel->getId(), $request->request->get('_token'))) {
@@ -94,7 +94,7 @@ class TravelController extends AbstractController
      * @throws ORMException
      */
 
-    #[Route('/register/{id}',name: 'app_travel_register' )]
+    #[Route('/register/{id}',name: 'register' )]
     public function register(
         EntityManagerInterface $entityManager,
         $id,
@@ -114,11 +114,11 @@ class TravelController extends AbstractController
 
         if ($statusId!=2)
         {
-            $this->addFlash('warning', 'STATUS ERROR : You cannot register to this travel it is not open for registration .');
+            $this->addFlash('warning', 'STATUS ERROR : You cannot be register to this travel it is not open for registration .');
         }else {
             foreach ($travelToRegister->getSubscriptionedTravelers() as $traveler) {
                 if ($traveler->getUserIdentifier() === $currentUser->getUserIdentifier()){
-                    $this->addFlash('warning', 'ALREADY REGISTER ERROR : You have already registered for this travel');
+                    $this->addFlash('warning', 'ALREADY REGISTERED ERROR : You have already been registered for this travel');
                     $registered = true;
                 }
             }
@@ -126,13 +126,13 @@ class TravelController extends AbstractController
                 $nbTravelers = count($travelToRegister->getSubscriptionedTravelers());
                 $maxtraveler = $travelToRegister->getNbMaxTraveler();
                 if ($nbTravelers >= $maxtraveler) {
-                    $this->addFlash('warning', 'TRAVELERS ERROR : You cannot register to this travel : the maximum travelers has been reached');
+                    $this->addFlash('warning', 'TRAVELERS ERROR : You cannot be register to this travel : the maximum travelers has been reached');
                     $maxTravelersReached = true;
                 } else {
                     $travelToRegister->addSubscriptionedTraveler($currentUser);
                     $entityManager->persist($travelToRegister);
                     $entityManager->flush();
-                    $this->addFlash('success', 'You have registered for this travel');
+                    $this->addFlash('success', 'You have been registered for this travel');
                 }
             }
 
@@ -140,7 +140,32 @@ class TravelController extends AbstractController
         return $this->redirectToRoute('app_travel_index');
     }
 
-    #[Route("/{id}/cancel'", name: 'app_travel_canceltravel', methods: ['GET', 'POST'])]
+
+    #[Route('/unregister/{id}', name: 'unregister')]
+    public function unregister(
+        $id,
+        EntityManagerInterface $entityManager,
+        TravelRepository $travelRepository
+    ): Response
+    {
+        $currentUser = $this->getUser();
+        $travelToUnsubscribe = $travelRepository->find($id);
+
+        if ($travelToUnsubscribe->getSubscriptionedTravelers()->contains($currentUser)){
+            $travelToUnsubscribe->removeSubscriptionedTraveler($currentUser);
+            $entityManager->persist($travelToUnsubscribe);
+            $entityManager->flush();
+
+            $this->addFlash('succes', 'Your registration have been canceled');
+        }
+
+
+        return $this->redirectToRoute('app_travel_index');
+    }
+
+
+
+    #[Route("/{id}/cancel'", name: 'cancel_travel', methods: ['GET', 'POST'])]
     public function cancelTravel(Travel $travel, Request $request, TravelRepository $travelRepository): Response
     {
         $form = $this->createForm(TravelType::class, $travel);
