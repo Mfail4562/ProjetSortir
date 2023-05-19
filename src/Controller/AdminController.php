b9e1a96a0b0d700bdfe453199879528ccd412ba5
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-05-19 12:52:44 +0200

    commit debut admin
    dashboard et perm

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin_dashboard')]
-    public function index(): Response
+    #[Route('/admin', name: 'app_admin_dashboard', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
