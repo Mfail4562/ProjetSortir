3a37fbabc4d7492b663d47776f47747d80aa9f28
mathieu <mathieu.perin.prog@gmail.com>
2023-05-22 14:48:17 +0200

    restore

--- src/Form/PlaceType.php
+++ src/Form/PlaceType.php
@@ -6,7 +6,6 @@ use App\Entity\City;
 use App\Entity\Place;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,17 +15,17 @@ class PlaceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class,[
-                'choice_label'=> 'name'
+            ->add('name', TextType::class, [
+                'label' => 'name'
             ])
-            ->add('street', TextType::class,[
-                'label'=>'street'
+            ->add('street', TextType::class, [
+                'label' => 'street'
             ])
-            ->add('latitude', TextType::class,[
-                'label'=>'latitude'
+            ->add('latitude', TextType::class, [
+                'label' => 'latitude'
             ])
-            ->add('longitude',TextType::class, [
-                'label'=> 'longitude'
+            ->add('longitude', TextType::class, [
+                'label' => 'longitude'
             ])
             ->add('city', EntityType::class, [
                 'class' => City::class,
