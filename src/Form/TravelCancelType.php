229564d1e5c62e227079283098e75a0c6b2899f4
Mfail4562 <mathieu.perin.prog@gmail.com>
2023-05-22 11:59:38 +0200

    Update TravelCancelType.php

--- src/Form/TravelCancelType.php
+++ src/Form/TravelCancelType.php
@@ -10,6 +10,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -37,7 +38,7 @@ class TravelCancelType extends AbstractType
 
                 ]
             ])
-            ->add('duration', DateTimeType::class, [
+            ->add('duration', TimeType::class, [
                 'label' => false,
                 'widget' => 'single_text',
                 'attr' => [
