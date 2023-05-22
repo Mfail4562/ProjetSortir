a8baf4a7cc409a060f2850c875752bdb0cdb1935
Mfail4562 <mathieu.perin.prog@gmail.com>
2023-05-22 12:00:32 +0200

    Update TravelType.php

--- src/Form/TravelType.php
+++ src/Form/TravelType.php
@@ -10,6 +10,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -32,7 +33,7 @@ class TravelType extends AbstractType
                 ]
             ])
 
-            ->add('duration',DateTimeType::class, [
+            ->add('duration',TimeType::class, [
 
                 'label' => 'Duration',
                 'widget'=> 'single_text',
