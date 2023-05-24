421d6302efdb6c336e02223f6d5cf6742a2e2a82
mathieu <mathieu.perin.prog@gmail.com>
2023-05-25 01:51:34 +0200

    resolution des minibug du au merge
    on avait casser la features de @HindElMouta sorry
    
    mais c'est réparé maintenant :D
    
    Co-Authored-By: Ragnarohf <64867475+Ragnarohf@users.noreply.github.com>

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,5 @@
 import './styles/app.scss';
 
 // You can specify which plugins you need
-import { Tooltip, Toast, Popover } from 'bootstrap';
-
 // start the Stimulus application
 import './bootstrap';
--- migrations/Version20230517141302.php
+++ migrations/Version20230517141302.php
@@ -1,31 +1,31 @@
 <?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace DoctrineMigrations;
+    namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\AbstractMigration;
+    use Doctrine\DBAL\Schema\Schema;
+    use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20230517141302 extends AbstractMigration
-{
-    public function getDescription(): string
+    /**
+     * Auto-generated Migration: Please modify to your needs!
+     */
+    final class Version20230517141302 extends AbstractMigration
     {
-        return '';
-    }
+        public function getDescription(): string
+        {
+            return '';
+        }
 
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE travel ADD cancel_message LONGTEXT DEFAULT NULL');
-    }
+        public function up(Schema $schema): void
+        {
+            // this up() migration is auto-generated, please modify it to your needs
+            $this->addSql('ALTER TABLE travel ADD cancel_message LONGTEXT DEFAULT NULL');
+        }
 
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE travel DROP cancel_message');
+        public function down(Schema $schema): void
+        {
+            // this down() migration is auto-generated, please modify it to your needs
+            $this->addSql('ALTER TABLE travel DROP cancel_message');
+        }
     }
-}
