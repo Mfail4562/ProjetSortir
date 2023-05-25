0ecbcb21c99895466ad7e2fbc91dba4f7ce2b26f
mathieu <mathieu.perin.prog@gmail.com>
2023-05-25 15:49:53 +0200

    oups desole
    reparation de bug

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -1,82 +1,88 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
 
-use App\Data\FindData;
-use App\Entity\Place;
-use App\Entity\Travel;
-use App\Form\FindType;
-use App\Form\TravelCancelType;
-use App\Form\TravelType;
-use App\Repository\StatusRepository;
-use App\Repository\TravelRepository;
-use App\Repository\UserRepository;
-use App\Service\RegisterService;
-use DateTime;
-use DateTimeZone;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Exception\ORMException;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Data\FindData;
+    use App\Entity\Place;
+    use App\Entity\Travel;
+    use App\Form\FindType;
+    use App\Form\TravelCancelType;
+    use App\Form\TravelType;
+    use App\Repository\StatusRepository;
+    use App\Repository\TravelRepository;
+    use App\Repository\UserRepository;
+    use App\Service\RegisterService;
+    use DateTime;
+    use DateTimeZone;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Doctrine\ORM\Exception\ORMException;
+    use Doctrine\Persistence\ManagerRegistry;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/travel', name: 'app_travel_')]
-class TravelController extends AbstractController
-{
-
-
-    #[Route('/', name: 'index', methods: ['GET', 'POST'])]
-    public function index(TravelRepository $travelRepository, StatusRepository $statusRepository, Request $request, EntityManagerInterface $emi): Response
-
+    #[Route('/travel', name: 'app_travel_')]
+    class TravelController extends AbstractController
     {
-        $data = new FindData();
-        $form = $this->createForm(FindType::class, $data);
-
-        $form->handleRequest($request);
 
 
-        $allTravelsForReturn = new ArrayCollection();
+        #[Route('/', name: 'index', methods: ['GET', 'POST'])]
+        public function index(TravelRepository $travelRepository, StatusRepository $statusRepository, Request $request, EntityManagerInterface $emi): Response
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userConnected = $this->getUser();
-            $data->setUserConnected($userConnected);
-            $allTravels = $travelRepository->findSearchTravel($data);
-            //dd($allTravels);
-        } else {
-            $allTravels = $travelRepository->findAll();
-        }
-        foreach ($allTravels as $travel) {
-            $secondsToAdd = $travel->getDuration()->getTimestamp();
-            $dateEnd = date('Y-m-d H:i:s', strtotime("+$secondsToAdd seconds", strtotime($travel->getDateStart()->format('Y-m-d H:i'))));
-            $now = date_format(new DateTime('now', new DateTimeZone('Europe/Paris')), 'Y-m-d H:i');
+        {
+            $data = new FindData();
+            $form = $this->createForm(FindType::class, $data);
 
-            if ($travel->getStatus()->getId() != 6) {
-                $newStatusId = 0;
+            $form->handleRequest($request);
 
 
-                if ($travel->getLimitDateSubscription()->format('Y-m-d H:i') < $now) { //cloturé
-                    $newStatusId = 3;
+            $allTravelsForReturn = new ArrayCollection();
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $userConnected = $this->getUser();
+                $data->setUserConnected($userConnected);
+                $allTravels = $travelRepository->findSearchTravel($data);
+                //dd($allTravels);
+            } else {
+                $allTravels = $travelRepository->findAll();
+            }
+            foreach ($allTravels as $travel) {
+                $secondsToAdd = $travel->getDuration()->getTimestamp();
+                $dateEnd = date('Y-m-d H:i:s', strtotime("+$secondsToAdd seconds", strtotime($travel->getDateStart()->format('Y-m-d H:i'))));
+                $now = date_format(new DateTime('now', new DateTimeZone('Europe/Paris')), 'Y-m-d H:i');
+
+                if ($travel->getStatus()->getId() != 6) {
+                    $newStatusId = 0;
+
+
+                    if ($travel->getLimitDateSubscription()->format('Y-m-d H:i') < $now) { //cloturé
+                        $newStatusId = 3;
+                    }
+
+                    if ($travel->getDateStart()->format('Y-m-d H:i') < $now && $dateEnd > $now) { //en cours
+                        $newStatusId = 4;
+                    }
+
+                    if ($dateEnd < $now) { //terminé
+                        $newStatusId = 5;
+                    }
+                    if ($newStatusId != 0) {
+                        $newStatus = $statusRepository->find($newStatusId);
+                        $travel->setStatus($newStatus);
+                        $travelRepository->save($travel, true);
+                    }
+
                 }
+                if (date('Y-m-d H:i', strtotime('+1 month', strtotime($dateEnd))) > $now) {
+                    $allTravelsForReturn->add($travel);
+                }
+            }
+            return $this->render('travel/index.html.twig', [
 
-                if ($travel->getDateStart()->format('Y-m-d H:i') < $now && $dateEnd > $now) { //en cours
-                    $newStatusId = 4;
-                }
-
-<<<<<<< Updated upstream
-                if ($dateEnd < $now) { //terminé
-                    $newStatusId = 5;
-                }
-                if ($newStatusId != 0) {
-                    $newStatus = $statusRepository->find($newStatusId);
-                    $travel->setStatus($newStatus);
-                    $travelRepository->save($travel, true);
-                }
-=======
                 'travels' => $allTravelsForReturn,
 
                 'form' => $form->createView(),
@@ -95,7 +101,6 @@ class TravelController extends AbstractController
             $travel->setLeader($user);
 
             $form = $this->createForm(TravelType::class, $travel);
-
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
@@ -135,190 +140,123 @@ class TravelController extends AbstractController
                     "id" => $place->getId(),
                     "name" => $place->getName()
                 );
->>>>>>> Stashed changes
 
             }
-            if (date('Y-m-d H:i', strtotime('+1 month', strtotime($dateEnd))) > $now) {
-                $allTravelsForReturn->add($travel);
+            return new JsonResponse($responseArray);
+        }
+
+        #[Route('/{id}', name: 'show', methods: ['GET'])]
+        public function show(Travel $travel): Response
+        {
+            return $this->render('travel/show.html.twig', [
+                'travel' => $travel,
+            ]);
+        }
+
+        #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+        public function edit(Request $request, Travel $travel, TravelRepository $travelRepository): Response
+        {
+            $travel->setPlace(null);
+            $form = $this->createForm(TravelType::class, $travel);
+            $form->handleRequest($request);
+
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $travelRepository->save($travel, true);
+
+                return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
             }
+
+            return $this->render('travel/edit.html.twig', [
+                'travel' => $travel,
+                'form' => $form,
+            ]);
         }
-        return $this->render('travel/index.html.twig', [
 
-            'travels' => $allTravelsForReturn,
-
-            'form' => $form->createView(),
-
-        ]);
-    }
-
-    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, TravelRepository $travelRepository, RegisterService $registerService, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
-    {
-        define('CREATING_MESSAGE', 'creation et inscription automatique :');
-        $user = $userRepository->find($this->getUser()->getUserIdentifier());
-
-
-        $travel = new Travel();
-        $travel->setLeader($user)
-            ->setDateStart(new DateTime('now', new DateTimeZone('Europe/Paris')));
-
-        $form = $this->createForm(TravelType::class, $travel);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $travel->setCampusOrganiser($user->getUserCampus());
-
-
-            $travelRepository->save($travel, true);
-
-            $registerService->RegisterToTravel($entityManager, $travel->getId(), $travelRepository, $user, $request, CREATING_MESSAGE);
+        #[Route('/{id}', name: 'delete', methods: ['POST'])]
+        public function delete(Request $request, Travel $travel, TravelRepository $travelRepository): Response
+        {
+            if ($this->isCsrfTokenValid('delete' . $travel->getId(), $request->request->get('_token'))) {
+                $travelRepository->remove($travel, true);
+            }
 
             return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        /**
+         * @throws ORMException
+         */
+        #[Route('/register/{id}', name: 'register')]
+        public function register(
+            EntityManagerInterface $entityManager,
+                                   $id,
+            TravelRepository       $travelRepository,
+            RegisterService        $registerService,
+            Request                $request,
+        ): Response
 
-        return $this->render('travel/new.html.twig', [
-            'travel' => $travel,
-            'form' => $form->createView(),
-        ]);
-    }
+        {
+            define('REGISTERING_MESSAGE', 'inscription :');
+            $currentUser = $this->getUser();
 
-    #[Route("/placeByCity", name: 'listplacesbycity', methods: ["GET", "POST"])]
-    public function listPlacesByCityAction(Request $request, ManagerRegistry $doctrine): JsonResponse
-    {
+            $registerService->RegisterToTravel($entityManager, $id, $travelRepository, $currentUser, $request, REGISTERING_MESSAGE);
 
-        $em = $doctrine->getManager();
-        $placeRepository = $em->getRepository(Place::class);
-
-        $places = $placeRepository->createQueryBuilder('p')
-            ->where('p.city=:city')
-            ->setParameter("city", $request->query->get("city"))
-            ->getQuery()
-            ->getResult();
-
-        $responseArray = array();
-        foreach ($places as $place) {
-            $responseArray[] = array(
-                "id" => $place->getId(),
-                "name" => $place->getName()
-            );
-
-        }
-        return new JsonResponse($responseArray);
-    }
-
-    #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(Travel $travel): Response
-    {
-        return $this->render('travel/show.html.twig', [
-            'travel' => $travel,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Travel $travel, TravelRepository $travelRepository): Response
-    {
-        $travel->setPlace(null);
-        $form = $this->createForm(TravelType::class, $travel);
-        $form->handleRequest($request);
-
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $travelRepository->save($travel, true);
-
-            return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('travel/edit.html.twig', [
-            'travel' => $travel,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'delete', methods: ['POST'])]
-    public function delete(Request $request, Travel $travel, TravelRepository $travelRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $travel->getId(), $request->request->get('_token'))) {
-            $travelRepository->remove($travel, true);
-        }
-
-        return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
-    }
-
-    /**
-     * @throws ORMException
-     */
-    #[Route('/register/{id}', name: 'register')]
-    public function register(
-        EntityManagerInterface $entityManager,
-                               $id,
-        TravelRepository       $travelRepository,
-        RegisterService        $registerService,
-        Request                $request,
-    ): Response
-
-    {
-        define('REGISTERING_MESSAGE', 'inscription :');
-        $currentUser = $this->getUser();
-
-        $registerService->RegisterToTravel($entityManager, $id, $travelRepository, $currentUser, $request, REGISTERING_MESSAGE);
-
-        return $this->redirectToRoute('app_travel_index');
-    }
-
-
-    #[Route('/unregister/{id}', name: 'unregister')]
-    public function unregister(
-        $id,
-        EntityManagerInterface $entityManager,
-        TravelRepository $travelRepository,
-        RegisterService $registerService
-    ): Response
-    {
-
-        define('UNREGISTERING_MESSAGE', 'désinscription :');
-        $currentUser = $this->getUser();
-        $travelToUnsubscribe = $travelRepository->find($id);
-
-        if ($travelToUnsubscribe->getSubscriptionedTravelers()->contains($currentUser)) {
-            $travelToUnsubscribe->removeSubscriptionedTraveler($currentUser);
-            $entityManager->persist($travelToUnsubscribe);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Your registration have been canceled');
-
-            $registerService->addToTxtFollowing(UNREGISTERING_MESSAGE, $currentUser, $travelToUnsubscribe);
-
+            return $this->redirectToRoute('app_travel_index');
         }
 
 
-        return $this->redirectToRoute('app_travel_index');
-    }
+        #[Route('/unregister/{id}', name: 'unregister')]
+        public function unregister(
+            $id,
+            EntityManagerInterface $entityManager,
+            TravelRepository $travelRepository,
+            RegisterService $registerService
+        ): Response
+        {
+
+            define('UNREGISTERING_MESSAGE', 'désinscription :');
+            $currentUser = $this->getUser();
+            $travelToUnsubscribe = $travelRepository->find($id);
+
+            if ($travelToUnsubscribe->getSubscriptionedTravelers()->contains($currentUser)) {
+                $travelToUnsubscribe->removeSubscriptionedTraveler($currentUser);
+                $entityManager->persist($travelToUnsubscribe);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Your registration have been canceled');
+
+                $registerService->addToTxtFollowing(UNREGISTERING_MESSAGE, $currentUser, $travelToUnsubscribe);
+
+            }
 
 
-    #[Route("/cancel/{id}", name: 'cancel_travel', methods: ['GET', 'POST'])]
-    public function cancelTravel(Travel $travel, Request $request, TravelRepository $travelRepository, StatusRepository $statusRepository,): Response
-    {
-
-        $form = $this->createForm(TravelCancelType::class, $travel);
-
-        $form->handleRequest($request);
-
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $cancelStatusId = 6;
-            $cancelStatus = $statusRepository->find($cancelStatusId);
-            $travel->setStatus($cancelStatus);
-            $travelRepository->save($travel, true);
-
-            return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_travel_index');
         }
 
-        return $this->render('travel/cancel.html.twig', [
-            'travel' => $travel,
-            'form' => $form->createView(),
-        ]);
 
+        #[Route("/cancel/{id}", name: 'cancel_travel', methods: ['GET', 'POST'])]
+        public function cancelTravel(Travel $travel, Request $request, TravelRepository $travelRepository, StatusRepository $statusRepository,): Response
+        {
+
+            $form = $this->createForm(TravelCancelType::class, $travel);
+
+            $form->handleRequest($request);
+
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $cancelStatusId = 6;
+                $cancelStatus = $statusRepository->find($cancelStatusId);
+                $travel->setStatus($cancelStatus);
+                $travelRepository->save($travel, true);
+
+                return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('travel/cancel.html.twig', [
+                'travel' => $travel,
+                'form' => $form->createView(),
+            ]);
+
+        }
     }
-}
 
