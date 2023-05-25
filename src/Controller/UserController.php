8748126400d3568308014b6d8f001d8279ba6472
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-05-25 10:49:14 +0200

    ajout userAvatar +menage controller +type

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,106 +1,106 @@
 <?php
 
-    namespace App\Controller;
+namespace App\Controller;
 
-    use App\Entity\User;
-    use App\Form\UserType;
-    use App\Repository\UserRepository;
-    use App\Service\UserService;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\File\Exception\FileException;
-    use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
-    use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-    #[Route('/user')]
-    class UserController extends AbstractController
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        #[Route('/', name: 'app_user_index', methods: ['GET'])]
-        public function index(UserRepository $userRepository): Response
-        {
-            return $this->render('user/index.html.twig', [
-                'users' => $userRepository->findAll(),
-            ]);
-        }
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
 
-        #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-        public function show(int $id, UserRepository $userRepository): Response
-        {
-            $user = $userRepository->find($id);
-            return $this->render('user/show.html.twig', [
-                'user' => $user,
-            ]);
-        }
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(int $id, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
 
-        #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-        public function edit(Request $request, User $user, UserRepository $userRepository, SluggerInterface $slugger, UserService $userService, $id): Response
-        {
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserRepository $userRepository, UserService $userService, $id): Response
+    {
 
-            //dd($user);
+        //dd($user);
 
-            $form = $this->createForm(UserType::class, $user);
-            $form->handleRequest($request);
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-            if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
 
-                $avatarFile = $form->get('avatar')->getData();
-                if ($avatarFile) {
+            $avatarFile = $form->get('avatar')->getData();
+            if ($avatarFile) {
 
-                    $fileName = 'avatarUser' . $user->getId() . '.webp';
+                $fileName = 'avatarUser' . $user->getId() . '.webp';
 
-                    try {
-                        $avatarFile->move(
-                            $this->getParameter('avatar_directory'),
-                            $fileName
-                        );
-                    } catch (FileException $e) {
-
-                    }
-                    $user->setAvatar($fileName);
+                try {
+                    $avatarFile->move(
+                        $this->getParameter('avatar_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
 
                 }
+                $user->setAvatar($fileName);
 
-                $userRepository->save($user, true);
-
-
-                return $this->redirectToRoute('app_user_show', ['id' => $id], Response::HTTP_SEE_OTHER);
             }
 
-            return $this->render('user/edit.html.twig', [
-                'user' => $user,
-                'form' => $form->createView(),
-            ]);
+            $userRepository->save($user, true);
+
+
+            return $this->redirectToRoute('app_user_show', ['id' => $id], Response::HTTP_SEE_OTHER);
         }
 
-        #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-        public function delete(Request $request, User $user, UserRepository $userRepository): Response
-        {
-            if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
-                $userRepository->remove($user, true);
-            }
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
-
-        #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-        public function new(Request $request, UserRepository $userRepository): Response
-        {
-            $user = new User();
-            $form = $this->createForm(UserType::class, $user);
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $userRepository->save($user, true);
-
-                return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-            }
-
-            return $this->render('user/new.html.twig', [
-                'user' => $user,
-                'form' => $form->createView(),
-            ]);
-        }
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
+}
--- src/Form/TravelType.php
+++ src/Form/TravelType.php
@@ -1,155 +1,147 @@
 <?php
 
-    namespace App\Form;
+namespace App\Form;
 
-    use App\Entity\City;
-    use App\Entity\Place;
-    use App\Entity\Status;
-    use App\Entity\Travel;
-    use Doctrine\ORM\EntityManagerInterface;
-    use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-    use Symfony\Component\Form\AbstractType;
-    use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-    use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-    use Symfony\Component\Form\Extension\Core\Type\DateType;
-    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-    use Symfony\Component\Form\Extension\Core\Type\TextType;
-    use Symfony\Component\Form\Extension\Core\Type\TimeType;
-    use Symfony\Component\Form\FormBuilderInterface;
-    use Symfony\Component\Form\FormEvent;
-    use Symfony\Component\Form\FormEvents;
-    use Symfony\Component\Form\FormInterface;
-    use Symfony\Component\OptionsResolver\OptionsResolver;
-    use Symfony\Component\Validator\Constraints\GreaterThan;
-    use Symfony\Component\Validator\Constraints\Range;
+use App\Entity\City;
+use App\Entity\Place;
+use App\Entity\Status;
+use App\Entity\Travel;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\Range;
 
-    class TravelType extends AbstractType
+class TravelType extends AbstractType
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        private EntityManagerInterface $entityManager;
+        $this->entityManager = $entityManager;
+    }
 
-        public function __construct(EntityManagerInterface $entityManager)
-        {
-            $this->entityManager = $entityManager;
-        }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
 
-        public function buildForm(FormBuilderInterface $builder, array $options): void
-        {
-
-            $builder
-                ->add('name', TextType::class, [
-                    'label' => 'Nom de la sortie',
-                    'required' => true,
-                    'attr' => [
-                        'class' => 'form-control mb-2'
-                    ],
-                ])
-                ->add('dateStart', DateTimeType::class, [
-                    'label' => 'Date de début',
-                    'html5' => true,
-                    'widget' => 'single_text',
-                    'input_format' => 'd-m-Y H:m',
-                    'attr' => [
-                        'class' => 'form-control datetimepicker',
-                    ],
-                    'constraints' => [
-                        new GreaterThan([
-                            'value' => new \DateTime()
-                        ])
-                    ]
-                ])
-                ->add('duration', TimeType::class, [
-                    'label' => 'Duration',
-                    'widget' => 'choice',
-                ])
-                ->add('limitDateSubscription', DateType::class, [
-                    'widget' => 'single_text'])
-                ->add('nbMaxTraveler', ChoiceType::class, [
-                    'label' => 'Maximum number of travelers',
-                    'choices' => array_combine(range(0, 50), range(0, 50)),
-                    'constraints' => [
-                        new Range([
-                            'min' => 0,
-                            'max' => 50,
-                        ])
-                    ]
-                ])
-                ->add('infos', TextareaType::class, [
-                    'label' => 'Description'
-                ])
-                ->add('status', EntityType::class, [
-                    'class' => Status::class,
-                    'choice_label' => 'wording',
-                    'placeholder' => '--Choice any status--',
-
-                ])
-                ->add('place', EntityType::class, [
-                    'class' => Place::class,
-                    'choice_label' => 'name',
-                    'placeholder' => 'Choose a place',
-                    'required' => false,
-
-                ]);
-
-
-            $builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'));
-            $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'onPreSubmit'));
-
-        }
-
-
-        public function configureOptions(OptionsResolver $resolver): void
-        {
-            $resolver->setDefaults([
-                'data_class' => Travel::class,
-            ]);
-        }
-
-//
-        function onPreSubmit(FormEvent $event): void
-        {
-            $form = $event->getForm();
-            $data = $event->getData();
-            $city = $this->entityManager->getRepository(City::class)->find($data['city']);
-            $this->addElements($form, $city);
-
-        }
-
-        protected function addElements(FormInterface $form, City $city = null): void
-        {
-            $form->add('city', EntityType::class, array(
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la sortie',
                 'required' => true,
-                'data' => $city,
-                'placeholder' => 'Select a City...',
-                'class' => City::class,
+                'attr' => [
+                    'class' => 'form-control mb-2'
+                ],
+            ])
+            ->add('dateStart', DateTimeType::class, [
+                'label' => 'Date de début',
+                'html5' => true,
+                'widget' => 'single_text',
+                'input_format' => 'd-m-Y H:m',
+                'attr' => [
+                    'class' => 'form-control datetimepicker',
+                ],
+                'constraints' => [
+                    new GreaterThan([
+                        'value' => new DateTime()
+                    ])
+                ]
+            ])
+            ->add('duration', TimeType::class, [
+                'label' => 'Duration',
+                'widget' => 'choice',
+            ])
+            ->add('limitDateSubscription', DateType::class, [
+                'widget' => 'single_text'])
+            ->add('nbMaxTraveler', ChoiceType::class, [
+                'label' => 'Maximum number of travelers',
+                'choices' => array_combine(range(0, 50), range(0, 50)),
+                'constraints' => [
+                    new Range([
+                        'min' => 0,
+                        'max' => 50,
+                    ])
+                ]
+            ])
+            ->add('infos', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('status', EntityType::class, [
+                'class' => Status::class,
+                'choice_label' => 'wording',
+                'placeholder' => '--Choice any status--',
+
+            ])
+            ->add('place', EntityType::class, [
+                'class' => Place::class,
                 'choice_label' => 'name',
-                'mapped' => false));
+                'placeholder' => 'Choose a place',
+                'required' => false,
 
-            $places = array();
+            ]);
 
-//        if ($city) {
-//            $repoPlace = $this->entityManager->getRepository(Place::class);
-//            $places = $repoPlace->createQueryBuilder('p')
-//                ->where("p.city= city")
-//                ->setParameter('city', $city)
-//                ->getQuery()
-//                ->getResult();
-//
-//        }
 
-        }
-
-        function onPreSetData(FormEvent $event): void
-        {
-            $form = $event->getForm();
-            $travel = $event->getData();
-
-            $city = $travel->getPlace() ? $travel->getPlace()->getCity() : null;
-            $this->addElements($form, $city);
-        }
-
-        public function getBlockPrefix(): string
-        {
-            return 'AppEntity_Place';
-        }
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'));
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'onPreSubmit'));
 
     }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Travel::class,
+        ]);
+    }
+
+//
+    function onPreSubmit(FormEvent $event): void
+    {
+        $form = $event->getForm();
+        $data = $event->getData();
+        $city = $this->entityManager->getRepository(City::class)->find($data['city']);
+        $this->addElements($form, $city);
+
+    }
+
+    protected function addElements(FormInterface $form, City $city = null): void
+    {
+        $form->add('city', EntityType::class, array(
+            'required' => true,
+            'data' => $city,
+            'placeholder' => 'Select a City...',
+            'class' => City::class,
+            'choice_label' => 'name',
+            'mapped' => false));
+
+        $places = array();
+
+
+    }
+
+    function onPreSetData(FormEvent $event): void
+    {
+        $form = $event->getForm();
+        $travel = $event->getData();
+
+        $city = $travel->getPlace() ? $travel->getPlace()->getCity() : null;
+        $this->addElements($form, $city);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'AppEntity_Place';
+    }
+
+}
